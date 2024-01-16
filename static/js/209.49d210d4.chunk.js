620f52718dfa2f8ddc9e01885f815bf56457a90f
Tania66 <113130993+Tania66@users.noreply.github.com>
2024-01-16 19:54:53 +0000

    Deploying to gh-pages from @ Tania66/my-phoneBook@39d53433eb8df8b0be9b75f7c01c021cbbb0fad2 🚀

---
+++ static/js/209.49d210d4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[209],{8209:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("h1",{children:"Phonebook"})})}}}]);
+//# sourceMappingURL=209.49d210d4.chunk.js.map
